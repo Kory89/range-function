8b2a6af378235f58b23b4c038d989580ba9d573e
Kory89 <116244018+Kory89@users.noreply.github.com>
2023-01-07 19:13:59 +0200

    Add files via upload

---
+++ range().py
@@ -0,0 +1,8 @@
+#--------------------------------------------------------------------------------
+# we use a range() function to return a sequence of numbers, starting from 0 by default, 
+# and increment by 1 (by default), and stop before a specified number.
+#--------------------------------------------------------------------------------
+
+
+for number in range(4, 8, 2):
+    print(number)
